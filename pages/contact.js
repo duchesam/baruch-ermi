029c8713c3a190ff90290feeaaaf2ac1d27464aa
duchesam <78324845+duchesam@users.noreply.github.com>
2026-05-28 13:47:33 -0400

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -11,7 +11,7 @@ export default function Contact(){
           <p>We move fast and keep things simple.</p>
           <p><strong>Phone:</strong> <a href="tel:+18335233104">(833) 523-3104</a></p>
           <p><strong>Email:</strong> <a href="mailto:admin@baruchermi.org">admin@baruchermi.org</a></p>
-          <p className="small">Baruc-Ermi — Nationwide foreclosure & auction solutions.</p>
+          <p className="small">Baruch-Ermi — Nationwide foreclosure & auction solutions.</p>
         </div>
       </main>
       <Footer />
