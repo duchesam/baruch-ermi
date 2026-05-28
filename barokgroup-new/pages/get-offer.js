5fa56ee7f3dd9dc122910eaacf21d8fe06d16b45
duchesam <78324845+duchesam@users.noreply.github.com>
2026-05-28 13:59:27 -0400

    Update get-offer.js

--- barokgroup-new/pages/get-offer.js
+++ barokgroup-new/pages/get-offer.js
@@ -8,7 +8,7 @@ export default function GetOffer(){
       <main className="section">
         <div className="container card" style={{maxWidth:"840px"}}>
           <h1 style={{marginTop:0}}>Get an Offer</h1>
-          <p className="small">Email your address and a brief timeline to <a href="mailto:hello@barokgroup.org">hello@barokgroup.org</a> or call <a href="tel:+15712753720">(571) 275-3720</a>. We’ll respond quickly.</p>
+          <p className="small">Email your address and a brief timeline to <a href="mailto:admin@baruchermi.org">admin@baruchermi.org</a> or call <a href="tel:+18335233104">(833) 523-3104</a>. We’ll respond quickly.</p>
         </div>
       </main>
       <Footer />
