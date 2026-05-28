e51b9e4a256504829767fd245b0e5562f4b5f549
duchesam <78324845+duchesam@users.noreply.github.com>
2026-05-28 13:23:13 -0400

    Update get-offer.js

--- pages/get-offer.js
+++ pages/get-offer.js
@@ -8,7 +8,7 @@ export default function GetOffer(){
       <main className="section">
         <div className="container card" style={{maxWidth:"900px"}}>
           <h1>Get a Cash Offer</h1>
-          <p className="small">Email <a href="mailto:hello@barokgroup.org">hello@barokgroup.org</a> or call <a href="tel:+15712753720">(571) 275-3720</a> and we’ll confirm your timeline and next steps.</p>
+          <p className="small">Email <a href="mailto:admin@baruchermi.org">admin@baruchermi.org</a> or call <a href="tel:+18335233104">(833) 523-3104</a> and we’ll confirm your timeline and next steps.</p>
         </div>
       </main>
       <Footer />
